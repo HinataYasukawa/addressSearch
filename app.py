5982a853e5517b1d96d02a781207849749de4577
HinataYasukawa <h1i2n2a62208@gmail.com>
2023-07-10 10:36:35 +0900

    make default function

---
+++ app.py
@@ -0,0 +1,18 @@
+import csv
+
+def search_address(zip_code):
+    with open('utf_all.csv', 'r') as f:
+        reader = csv.reader(f)
+        next(reader)  # Skip the header row
+        for row in reader:
+            if row[2] == zip_code:
+                return f"{row[6]} {row[7]} {row[8]}"
+    return "No address found for this postal code."
+
+def main():
+    zip_code = input("Please enter a postal code: ")
+    address = search_address(zip_code.replace("-", ""))  # Remove dashes if they exist
+    print(address)
+
+if __name__ == "__main__":
+    main()
