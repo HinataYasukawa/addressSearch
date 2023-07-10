6fe7b1d8bc95b17284f90264e4d33f9220b36b83
HinataYasukawa <h1i2n2a62208@gmail.com>
2023-07-10 13:07:36 +0900

    jump for googlemap

--- app.py
+++ app.py
@@ -1,29 +1,28 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, jsonify, render_template
 import csv
 
 app = Flask(__name__)
 
 
-def search_address(zip_code):
-    with open("utf_all.csv", encoding="utf-8", mode="r") as f:
+def find_address(postcode):
+    with open("utf_all.csv", encoding="utf-8") as f:
         reader = csv.reader(f)
-        next(reader)
         for row in reader:
-            if row[2] == zip_code:
+            if row[2] == postcode:
                 return f"{row[6]} {row[7]} {row[8]}"
-    return "No address found for this postal code."
+    return "Address not found"
+
+
+@app.route("/search", methods=["GET"])
+def search():
+    postcode = request.args.get("postcode", "")
+    address = find_address(postcode)
+    return jsonify({"address": address})
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
-
-
-@app.route("/search", methods=["POST"])
-def search():
-    zip_code = request.form.get("zipcode")
-    address = search_address(zip_code.replace("-", ""))
-    return address
 
 
 if __name__ == "__main__":
--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,15 @@
-$(document).ready(function(){
-    $("#search-button").click(function(){
-        $.post("/search",
-        {
-            zipcode: $("#zipcode").val()
-        },
-        function(data, status){
-            $("#result").text("Address: " + data);
+function search() {
+    var postcode = document.getElementById('postcode').value;
+    fetch('/search?postcode=' + postcode)
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('address').textContent = data.address;
+            document.getElementById('openMap').style.display = "block";
         });
-    });
-});
+}
+
+function openMap() {
+    var address = document.getElementById('address').textContent;
+    var url = "https://www.google.com/maps/search/?api=1&query=" + encodeURIComponent(address);
+    window.open(url, '_blank');
+}
